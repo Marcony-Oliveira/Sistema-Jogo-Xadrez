2e08f4b74fdca914f4f313afbf21acdbc29816da
Marcony Oliveira <marcony-oliveira@hotmail.com>
2019-05-07 21:07:07 -0300

    Criada a classe posição

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Posicao posicao = new Posicao(3,5);
+		System.out.println(posicao);
 
 	}
 
