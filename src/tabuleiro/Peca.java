fae750b7d7c1598cedfc9b7218c117359eac0884
Marcony Oliveira <marcony-oliveira@hotmail.com>
2019-05-07 21:24:01 -0300

    Começando a implementar as classes Tabuleiro e Peças

---
+++ src/tabuleiro/Peca.java
@@ -0,0 +1,29 @@
+package tabuleiro;
+
+public class Peca {
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		super();
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
