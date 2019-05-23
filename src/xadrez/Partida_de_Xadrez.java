d3f25d886389dd23c2203ff91c0096a41e47e20b
Marcony Oliveira <marcony-oliveira@hotmail.com>
2019-05-23 20:45:58 -0300

    Jogada especial ENPASSANT

--- src/xadrez/Partida_de_Xadrez.java
+++ src/xadrez/Partida_de_Xadrez.java
@@ -21,175 +21,222 @@ public class Partida_de_Xadrez {
 	private Tabuleiro board;
 	private boolean check;
 	private boolean checkMate;
-	
+	private Peca_de_Xadrez enPassantVuneravel;
+
 	private List<Peca> piecesOnTheBoard = new ArrayList<>();
 	private List<Peca> capturedPieces = new ArrayList<>();
-	
+
 	public Partida_de_Xadrez() {
 		board = new Tabuleiro(8, 8);
 		turn = 1;
 		currentPlayer = Color.WHITE;
 		config_Inicial();
 	}
-	
+
 	public int getTurn() {
 		return turn;
 	}
-	
+
 	public Color getCurrentPlayer() {
 		return currentPlayer;
 	}
-	
+
 	public boolean getCheck() {
 		return check;
 	}
-	
+
 	public boolean getCheckMate() {
 		return checkMate;
 	}
-	
+
+	public Peca_de_Xadrez enPassantVuneravel() {
+		return enPassantVuneravel;
+	}
+
 	public Peca_de_Xadrez[][] getPieces() {
 		Peca_de_Xadrez[][] mat = new Peca_de_Xadrez[board.getLinhas()][board.getColunas()];
-		for (int i=0; i<board.getLinhas(); i++) {
-			for (int j=0; j<board.getColunas(); j++) {
+		for (int i = 0; i < board.getLinhas(); i++) {
+			for (int j = 0; j < board.getColunas(); j++) {
 				mat[i][j] = (Peca_de_Xadrez) board.pecas(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	public boolean[][] possibleMoves(XadrezPosicao sourcePosition) {
 		Posicao position = sourcePosition.toPosicao();
 		validateSourcePosition(position);
 		return board.peca(position).movimentosPossiveis();
 	}
-	
+
 	public Peca_de_Xadrez performChessMove(XadrezPosicao sourcePosition, XadrezPosicao targetPosition) {
 		Posicao source = sourcePosition.toPosicao();
 		Posicao target = targetPosition.toPosicao();
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Peca capturedPiece = makeMove(source, target);
-		
+
 		if (testCheck(currentPlayer)) {
 			undoMove(source, target, capturedPiece);
 			throw new XadrezExceptions("You can't put yourself in check");
 		}
-		
+
+		Peca_de_Xadrez pecaMovida = (Peca_de_Xadrez) board.peca(target);
+
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 
 		if (testCheckMate(opponent(currentPlayer))) {
 			checkMate = true;
-		}
-		else {
+		} else {
 			nextTurn();
 		}
-		
-		return (Peca_de_Xadrez)capturedPiece;
+
+		// Jogada especial en Passant
+		if (pecaMovida instanceof Peao
+				&& (target.getLinha() == source.getLinha() - 2 || target.getLinha() == source.getLinha() + 2)) {
+			enPassantVuneravel = pecaMovida;
+		} else {
+			enPassantVuneravel = null;
+		}
+
+		return (Peca_de_Xadrez) capturedPiece;
 	}
-	
+
 	private Peca makeMove(Posicao source, Posicao target) {
-		Peca_de_Xadrez p = (Peca_de_Xadrez)board.removePeca(source);
+		Peca_de_Xadrez p = (Peca_de_Xadrez) board.removePeca(source);
 		p.incrementaContagemMovimentos();
 		Peca capturedPiece = board.removePeca(target);
 		board.lugar_da_Peca(p, target);
-		
+
 		if (capturedPiece != null) {
 			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
 		}
-		
-		//Jogada especial Castling proximo do rei
-		if(p instanceof Rei && target.getColuna() == source.getColuna() + 2) {
-			Posicao sourceT = new Posicao (source.getLinha(), source.getColuna() + 3);
-			Posicao targetT = new Posicao (source.getLinha(), source.getColuna() + 1);
-			Peca_de_Xadrez torre = (Peca_de_Xadrez)board.removePeca(sourceT);
+
+		// Jogada especial Castling proximo do rei
+		if (p instanceof Rei && target.getColuna() == source.getColuna() + 2) {
+			Posicao sourceT = new Posicao(source.getLinha(), source.getColuna() + 3);
+			Posicao targetT = new Posicao(source.getLinha(), source.getColuna() + 1);
+			Peca_de_Xadrez torre = (Peca_de_Xadrez) board.removePeca(sourceT);
 			board.lugar_da_Peca(torre, targetT);
 			torre.incrementaContagemMovimentos();
 		}
-		//Jogada especial Castling distante do rei
-				if(p instanceof Rei && target.getColuna() == source.getColuna() - 2) {
-					Posicao sourceT = new Posicao (source.getLinha(), source.getColuna() - 4);
-					Posicao targetT = new Posicao (source.getLinha(), source.getColuna() - 1);
-					Peca_de_Xadrez torre = (Peca_de_Xadrez)board.removePeca(sourceT);
-					board.lugar_da_Peca(torre, targetT);
-					torre.incrementaContagemMovimentos();
+		// Jogada especial Castling distante do rei
+		if (p instanceof Rei && target.getColuna() == source.getColuna() - 2) {
+			Posicao sourceT = new Posicao(source.getLinha(), source.getColuna() - 4);
+			Posicao targetT = new Posicao(source.getLinha(), source.getColuna() - 1);
+			Peca_de_Xadrez torre = (Peca_de_Xadrez) board.removePeca(sourceT);
+			board.lugar_da_Peca(torre, targetT);
+			torre.incrementaContagemMovimentos();
+		}
+
+		// Jogada especial ENPASSANT
+		if (p instanceof Peao) {
+			if (source.getColuna() != target.getColuna() && capturedPiece == null) {
+				Posicao posicaoPeao;
+				if (p.getColor() == Color.WHITE) {
+					posicaoPeao = new Posicao(target.getLinha() + 1, target.getColuna());
+				} else {
+					posicaoPeao = new Posicao(target.getLinha() - 1, target.getColuna());
 				}
-		
+				capturedPiece = board.removePeca(posicaoPeao);
+				capturedPieces.add(capturedPiece);
+				piecesOnTheBoard.remove(capturedPiece);
+			}
+
+		}
+
 		return capturedPiece;
 	}
-	
-	
+
 	private void undoMove(Posicao source, Posicao target, Peca capturedPiece) {
-		Peca_de_Xadrez p = (Peca_de_Xadrez)board.removePeca(target);
+		Peca_de_Xadrez p = (Peca_de_Xadrez) board.removePeca(target);
 		p.decrementaContagemMovimentos();
 		board.lugar_da_Peca(p, source);
-		
+
 		if (capturedPiece != null) {
 			board.lugar_da_Peca(capturedPiece, target);
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
 		}
-		
-		//Jogada especial Castling proximo do rei
-				if(p instanceof Rei && target.getColuna() == source.getColuna() + 2) {
-					Posicao sourceT = new Posicao (source.getLinha(), source.getColuna() + 3);
-					Posicao targetT = new Posicao (source.getLinha(), source.getColuna() + 1);
-					Peca_de_Xadrez torre = (Peca_de_Xadrez)board.removePeca(targetT);
-					board.lugar_da_Peca(torre, sourceT);
-					torre.decrementaContagemMovimentos();
+
+		// Jogada especial Castling proximo do rei
+		if (p instanceof Rei && target.getColuna() == source.getColuna() + 2) {
+			Posicao sourceT = new Posicao(source.getLinha(), source.getColuna() + 3);
+			Posicao targetT = new Posicao(source.getLinha(), source.getColuna() + 1);
+			Peca_de_Xadrez torre = (Peca_de_Xadrez) board.removePeca(targetT);
+			board.lugar_da_Peca(torre, sourceT);
+			torre.decrementaContagemMovimentos();
+		}
+		// Jogada especial Castling distante do rei
+		if (p instanceof Rei && target.getColuna() == source.getColuna() - 2) {
+			Posicao sourceT = new Posicao(source.getLinha(), source.getColuna() - 4);
+			Posicao targetT = new Posicao(source.getLinha(), source.getColuna() - 1);
+			Peca_de_Xadrez torre = (Peca_de_Xadrez) board.removePeca(targetT);
+			board.lugar_da_Peca(torre, sourceT);
+			torre.decrementaContagemMovimentos();
+			;
+		}
+
+		// Jogada especial ENPASSANT
+		if (p instanceof Peao) {
+			if (source.getColuna() != target.getColuna() && capturedPiece == enPassantVuneravel) {
+				Peca_de_Xadrez peao = (Peca_de_Xadrez)board.removePeca(target);
+				Posicao posicaoPeao;
+				if (p.getColor() == Color.WHITE) {
+					posicaoPeao = new Posicao(3, target.getColuna());
+				} else {
+					posicaoPeao = new Posicao(4, target.getColuna());
 				}
-				//Jogada especial Castling distante do rei
-						if(p instanceof Rei && target.getColuna() == source.getColuna() - 2) {
-							Posicao sourceT = new Posicao (source.getLinha(), source.getColuna() - 4);
-							Posicao targetT = new Posicao (source.getLinha(), source.getColuna() - 1);
-							Peca_de_Xadrez torre = (Peca_de_Xadrez)board.removePeca(targetT);
-							board.lugar_da_Peca(torre, sourceT);
-							torre.decrementaContagemMovimentos();;
-						}
+				board.lugar_da_Peca(peao, posicaoPeao);
+				
+			}
+
+		}
 	}
-	
-	
+
 	private void validateSourcePosition(Posicao position) {
 		if (!board.temUmaPecaNaPosicao(position)) {
 			throw new XadrezExceptions("There is no piece on source position");
 		}
-		if (currentPlayer != ((Peca_de_Xadrez)board.peca(position)).getColor()) {
+		if (currentPlayer != ((Peca_de_Xadrez) board.peca(position)).getColor()) {
 			throw new XadrezExceptions("The chosen piece is not yours");
 		}
 		if (!board.peca(position).existeAlgumMovimentoPossivel()) {
 			throw new XadrezExceptions("There is no possible moves for the chosen piece");
 		}
 	}
-	
+
 	private void validateTargetPosition(Posicao source, Posicao target) {
 		if (!board.peca(source).movimentoPossivel(target)) {
 			throw new XadrezExceptions("The chosen piece can't move to target position");
 		}
 	}
-	
+
 	private void nextTurn() {
 		turn++;
 		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
-	
+
 	private Color opponent(Color color) {
 		return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
-	
+
 	private Peca_de_Xadrez king(Color color) {
-		List<Peca> list = piecesOnTheBoard.stream().filter(x -> ((Peca_de_Xadrez)x).getColor() == color).collect(Collectors.toList());
+		List<Peca> list = piecesOnTheBoard.stream().filter(x -> ((Peca_de_Xadrez) x).getColor() == color)
+				.collect(Collectors.toList());
 		for (Peca p : list) {
 			if (p instanceof Rei) {
-				return (Peca_de_Xadrez)p;
+				return (Peca_de_Xadrez) p;
 			}
 		}
 		throw new IllegalStateException("There is no " + color + " king on the board");
 	}
-	
+
 	private boolean testCheck(Color color) {
 		Posicao kingPosition = king(color).getXadrezPosicao().toPosicao();
-		List<Peca> opponentPieces = piecesOnTheBoard.stream().filter(x -> ((Peca_de_Xadrez)x).getColor() == opponent(color)).collect(Collectors.toList());
+		List<Peca> opponentPieces = piecesOnTheBoard.stream()
+				.filter(x -> ((Peca_de_Xadrez) x).getColor() == opponent(color)).collect(Collectors.toList());
 		for (Peca p : opponentPieces) {
 			boolean[][] mat = p.movimentosPossiveis();
 			if (mat[kingPosition.getLinha()][kingPosition.getColuna()]) {
@@ -198,18 +245,19 @@ public class Partida_de_Xadrez {
 		}
 		return false;
 	}
-	
+
 	private boolean testCheckMate(Color color) {
 		if (!testCheck(color)) {
 			return false;
 		}
-		List<Peca> list = piecesOnTheBoard.stream().filter(x -> ((Peca_de_Xadrez)x).getColor() == color).collect(Collectors.toList());
+		List<Peca> list = piecesOnTheBoard.stream().filter(x -> ((Peca_de_Xadrez) x).getColor() == color)
+				.collect(Collectors.toList());
 		for (Peca p : list) {
 			boolean[][] mat = p.movimentosPossiveis();
-			for (int i=0; i<board.getLinhas(); i++) {
-				for (int j=0; j<board.getColunas(); j++) {
+			for (int i = 0; i < board.getLinhas(); i++) {
+				for (int j = 0; j < board.getColunas(); j++) {
 					if (mat[i][j]) {
-						Posicao source = ((Peca_de_Xadrez)p).getXadrezPosicao().toPosicao();
+						Posicao source = ((Peca_de_Xadrez) p).getXadrezPosicao().toPosicao();
 						Posicao target = new Posicao(i, j);
 						Peca capturedPiece = makeMove(source, target);
 						boolean testCheck = testCheck(color);
@@ -222,51 +270,48 @@ public class Partida_de_Xadrez {
 			}
 		}
 		return true;
-	}	
-	
+	}
+
 	private void placeNewPiece(char column, int row, Peca_de_Xadrez piece) {
 		board.lugar_da_Peca(piece, new XadrezPosicao(column, row).toPosicao());
 		piecesOnTheBoard.add(piece);
 	}
-	
+
 	private void config_Inicial() {
-		
+
 		placeNewPiece('a', 1, new Torre(board, Color.WHITE));
 		placeNewPiece('b', 1, new Cavalo(board, Color.WHITE));
-        placeNewPiece('c', 1, new Bispo(board, Color.WHITE));
-        placeNewPiece('d', 1, new Rainha(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rei(board, Color.WHITE, this));
-        placeNewPiece('f', 1, new Bispo(board, Color.WHITE));
-        placeNewPiece('g', 1, new Cavalo(board, Color.WHITE));
-        placeNewPiece('h', 1, new Torre(board, Color.WHITE));
-        placeNewPiece('a', 2, new Peao(board, Color.WHITE));
-        placeNewPiece('b', 2, new Peao(board, Color.WHITE));
-        placeNewPiece('c', 2, new Peao(board, Color.WHITE));
-        placeNewPiece('d', 2, new Peao(board, Color.WHITE));
-        placeNewPiece('e', 2, new Peao(board, Color.WHITE));
-        placeNewPiece('f', 2, new Peao(board, Color.WHITE));
-        placeNewPiece('g', 2, new Peao(board, Color.WHITE));
-        placeNewPiece('h', 2, new Peao(board, Color.WHITE));
+		placeNewPiece('c', 1, new Bispo(board, Color.WHITE));
+		placeNewPiece('d', 1, new Rainha(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rei(board, Color.WHITE, this));
+		placeNewPiece('f', 1, new Bispo(board, Color.WHITE));
+		placeNewPiece('g', 1, new Cavalo(board, Color.WHITE));
+		placeNewPiece('h', 1, new Torre(board, Color.WHITE));
+		placeNewPiece('a', 2, new Peao(board, Color.WHITE, this));
+		placeNewPiece('b', 2, new Peao(board, Color.WHITE, this));
+		placeNewPiece('c', 2, new Peao(board, Color.WHITE, this));
+		placeNewPiece('d', 2, new Peao(board, Color.WHITE, this));
+		placeNewPiece('e', 2, new Peao(board, Color.WHITE, this));
+		placeNewPiece('f', 2, new Peao(board, Color.WHITE, this));
+		placeNewPiece('g', 2, new Peao(board, Color.WHITE, this));
+		placeNewPiece('h', 2, new Peao(board, Color.WHITE, this));
 
-        placeNewPiece('a', 8, new Torre(board, Color.BLACK));
-        placeNewPiece('b', 8, new Cavalo(board, Color.BLACK));
-        placeNewPiece('c', 8, new Bispo(board, Color.BLACK));
-        placeNewPiece('d', 8, new Rainha(board, Color.BLACK));
-        placeNewPiece('e', 8, new Rei(board, Color.BLACK, this));
-        placeNewPiece('f', 8, new Bispo(board, Color.BLACK));
-        placeNewPiece('g', 8, new Cavalo(board, Color.BLACK));
-        placeNewPiece('h', 8, new Torre(board, Color.BLACK));
-        placeNewPiece('a', 7, new Peao(board, Color.BLACK));
-        placeNewPiece('b', 7, new Peao(board, Color.BLACK));
-        placeNewPiece('c', 7, new Peao(board, Color.BLACK));
-        placeNewPiece('d', 7, new Peao(board, Color.BLACK));
-        placeNewPiece('e', 7, new Peao(board, Color.BLACK));
-        placeNewPiece('f', 7, new Peao(board, Color.BLACK));
-        placeNewPiece('g', 7, new Peao(board, Color.BLACK));
-        placeNewPiece('h', 7, new Peao(board, Color.BLACK));
-		}
-	
+		placeNewPiece('a', 8, new Torre(board, Color.BLACK));
+		placeNewPiece('b', 8, new Cavalo(board, Color.BLACK));
+		placeNewPiece('c', 8, new Bispo(board, Color.BLACK));
+		placeNewPiece('d', 8, new Rainha(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rei(board, Color.BLACK, this));
+		placeNewPiece('f', 8, new Bispo(board, Color.BLACK));
+		placeNewPiece('g', 8, new Cavalo(board, Color.BLACK));
+		placeNewPiece('h', 8, new Torre(board, Color.BLACK));
+		placeNewPiece('a', 7, new Peao(board, Color.BLACK, this));
+		placeNewPiece('b', 7, new Peao(board, Color.BLACK, this));
+		placeNewPiece('c', 7, new Peao(board, Color.BLACK, this));
+		placeNewPiece('d', 7, new Peao(board, Color.BLACK, this));
+		placeNewPiece('e', 7, new Peao(board, Color.BLACK, this));
+		placeNewPiece('f', 7, new Peao(board, Color.BLACK, this));
+		placeNewPiece('g', 7, new Peao(board, Color.BLACK, this));
+		placeNewPiece('h', 7, new Peao(board, Color.BLACK, this));
 	}
 
-
-
+}
--- src/xadrez/pecas/Peao.java
+++ src/xadrez/pecas/Peao.java
@@ -3,15 +3,19 @@ package xadrez.pecas;
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Color;
+import xadrez.Partida_de_Xadrez;
 import xadrez.Peca_de_Xadrez;
 
 public class Peao extends Peca_de_Xadrez {
 
-	public Peao(Tabuleiro tabuleiro, Color color) {
+	private Partida_de_Xadrez partidaDeXadrez;
+
+	public Peao(Tabuleiro tabuleiro, Color color, Partida_de_Xadrez partidaDeXadrez) {
 		super(tabuleiro, color);
+		this.partidaDeXadrez = partidaDeXadrez;
 
 	}
-	
+
 	@Override
 	public String toString() {
 		return "P";
@@ -46,6 +50,23 @@ public class Peao extends Peca_de_Xadrez {
 				mat[p.getLinha()][p.getColuna()] = true;
 
 			}
+
+			// Jogada especial ENPASSANT white
+			if (posicao.getLinha() == 3) {
+				Posicao esquerda = new Posicao(posicao.getLinha(), posicao.getColuna() - 1);
+				if (getTabuleiro().posicao_Existente(esquerda) && existePecaAdversaria(esquerda)
+						&& getTabuleiro().peca(esquerda) == partidaDeXadrez.enPassantVuneravel()) {
+					mat[esquerda.getLinha() - 1][esquerda.getColuna()] = true;
+				}
+				Posicao direita = new Posicao(posicao.getLinha(), posicao.getColuna() + 1);
+				if (getTabuleiro().posicao_Existente(direita) && existePecaAdversaria(direita)
+						&& getTabuleiro().peca(direita) == partidaDeXadrez.enPassantVuneravel()) {
+					mat[direita.getLinha() - 1][direita.getColuna()] = true;
+
+				}
+
+			}
+
 		} else {
 			p.setValues(posicao.getLinha() + 1, posicao.getColuna());
 			if (getTabuleiro().posicao_Existente(p) && !getTabuleiro().temUmaPecaNaPosicao(p)) {
@@ -69,8 +90,24 @@ public class Peao extends Peca_de_Xadrez {
 				mat[p.getLinha()][p.getColuna()] = true;
 			}
 			
+			// Jogada especial ENPASSANT black
+			if (posicao.getLinha() == 4) {
+				Posicao esquerda = new Posicao(posicao.getLinha(), posicao.getColuna() - 1);
+				if (getTabuleiro().posicao_Existente(esquerda) && existePecaAdversaria(esquerda)
+						&& getTabuleiro().peca(esquerda) == partidaDeXadrez.enPassantVuneravel()) {
+					mat[esquerda.getLinha() + 1][esquerda.getColuna()] = true;
+				}
+				Posicao direita = new Posicao(posicao.getLinha(), posicao.getColuna() + 1);
+				if (getTabuleiro().posicao_Existente(direita) && existePecaAdversaria(direita)
+						&& getTabuleiro().peca(direita) == partidaDeXadrez.enPassantVuneravel()) {
+					mat[direita.getLinha() + 1][direita.getColuna()] = true;
+
+				}
+
+			}
+
 		}
 		return mat;
-		
+
 	}
 }
